090f017205ec2a3c5cea482c76cf93f2f1328040
David Waring <djw64@cornell.edu>
2021-11-03 14:08:08 -0400

    Remove debugging logs

--- directus-endpoint-publish-netlify/src/index.js
+++ directus-endpoint-publish-netlify/src/index.js
@@ -12,15 +12,11 @@ const config = require('./config.js');
  * @returns {Integer} activity_id
  */
 async function _getActivityId(activityService) {
-    console.log("GET ACTIVITY ID:");
-    console.log(activityService);
-    console.log(config.activityFilter);
     let activity_rows = await activityService.readByQuery({ 
         filter: config.activityFilter, 
         sort: ["-timestamp"],
         limit: 1 
     });
-    console.log(activity_rows);
     return activity_rows && activity_rows.length === 1 ? activity_rows[0].id : undefined;
 }
 
@@ -188,35 +184,24 @@ module.exports = async function registerEndpoint(router, { services, env }) {
 
             // Check deploy sent via hook
             let build = req.body;
-            console.log("==> NETLIFY POST-DEPLOY HOOK:");
-            console.log("Build Site ID: " + build.site_id);
-            console.log("Build State: " + build.state);
             if ( build && build.site_id === site_id && build.state === 'ready' ) {
-                
+
                 // Get current metadata
                 let metadata = await _netlify_get(`/sites/${site_id}/metadata`);
                 let directus_metadata = metadata && metadata.directus ? metadata.directus : {};
 
-                console.log("Directus Metadata: " + JSON.stringify(directus_metadata));
-                
                 // Update activity in metadata
                 const activityService = new ActivityService({ schema: req.schema, accountability: req.accountability });
                 directus_metadata.lastActivityId = await _getActivityId(activityService);
                 metadata.directus = directus_metadata;
-
-                console.log("New Activity ID: " + directus_metadata.lastActivityId);
-                console.log("New Metadata: " + JSON.stringify(metadata));
-
                 await _netlify_put(`/sites/${site_id}/metadata`, metadata);
 
                 updated = true;
             }
 
-            console.log("UPDATED: " + updated);
             return res.send({ updated });
         }
         catch (error) {
-            console.log("ERROR: " + error.message);
             return res.send({ error: error.message });
         }
     });
